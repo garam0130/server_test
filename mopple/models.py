d98a0ae4e0f212b75c9152d56ebc49b445879e1e
student777 <darkdakku@gmail.com>
2015-10-10 13:14:07 +0900

    model-method 추가

--- mopple/models.py
+++ mopple/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
-# from datetime import datetime, timedelta
+from datetime import timedelta
 
 
 class UserInfo(models.Model):
@@ -13,17 +13,21 @@ class UserInfo(models.Model):
     def __str__(self):
         return '[U' + str(self.user.id) + ']' + self.user.username
 
+    def get_cum_panelty(self):
+        pass
+
 
 class GroupInfo(models.Model):
     group = models.OneToOneField(Group)
     deposit = models.CharField(max_length=20)
-    att_panelty = models.IntegerField(default=0)
-    ass_panelty = models.IntegerField(default=0)
+    att_penalty = models.IntegerField(default=0)  # per minute
+    ass_penalty = models.IntegerField(default=0)  # done or undone
     time_start = models.DateTimeField()
     time_end = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return '[G' + str(self.group.id) + ']' + self.group.name
+        # self.group.id != self.id
 
 
 class Place(models.Model):
@@ -48,23 +52,31 @@ class Place(models.Model):
 class Meeting(models.Model):
     title = models.CharField(max_length=20, blank=True, null=True)
     time = models.DateTimeField()
-    group = models.ForeignKey(Group)
+    group = models.ForeignKey(GroupInfo)  # group.id != groupinfo.id
     place = models.ForeignKey(Place, blank=True, null=True)
     homework = models.CharField(max_length=40, blank=True, null=True)  # to do at that day
 
     def __str__(self):
-        return '[U' + str(self.group.id) + ']' + str(self.time.date())
+        return '[G' + str(self.group.id) + ']' + str(self.time.date())
 
 
 class Attendance(models.Model):
     meeting = models.ForeignKey(Meeting)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(UserInfo)  # user.id != userinfo.id
     time_arrival = models.DateTimeField()
     done_hw = models.BooleanField(default=False)
 
     def __str__(self):
         return '[G' + str(self.meeting.group.id) + ']' + self.user.username
 
-    def get_delta(self):
+    def get_att_penalty(self):
         delta = self.meeting.time - self.time_arrival
-        return abs(delta).seconds
+        if delta <= timedelta(0):
+            return 0
+        else:
+            penalty = abs(delta).seconds/60*self.meeting.group.att_penalty
+            return penalty
+
+    def get_ass_penalty(self):
+        penalty = int(self.done_hw)*self.meeting.group.ass_penalty
+        return penalty
