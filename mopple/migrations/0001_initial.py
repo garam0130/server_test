f24f683b0d80281e60557cc85f2c535b69c4d779
student777 <darkdakku@gmail.com>
2015-10-31 03:31:19 +0900

    modify models.py , ignore migrations/ , TODO

--- mopple/migrations/0001_initial.py
+++ mopple/migrations/0001_initial.py
@@ -8,73 +8,80 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('auth', '0006_require_contenttypes_0002'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Attendance',
+            name='GroupInfo',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('time_arrival', models.DateTimeField()),
-                ('done_hw', models.BooleanField(default=False)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('balance', models.IntegerField(default=0)),
+                ('ass_rate', models.IntegerField(default=0)),
+                ('att_rate', models.IntegerField(default=0)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('cuppon', models.CharField(max_length=50, default='none')),
+                ('group', models.OneToOneField(to='auth.Group')),
             ],
         ),
         migrations.CreateModel(
-            name='GroupInfo',
+            name='Grouping',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('deposit', models.CharField(max_length=20)),
-                ('att_panelty', models.IntegerField(default=0)),
-                ('ass_panelty', models.IntegerField(default=0)),
-                ('time_start', models.DateTimeField()),
-                ('time_end', models.DateTimeField(null=True, blank=True)),
-                ('group', models.OneToOneField(to='auth.Group')),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('balance', models.IntegerField(default=0)),
+                ('group', models.ForeignKey(to='mopple.GroupInfo')),
             ],
         ),
         migrations.CreateModel(
             name='Meeting',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('title', models.CharField(max_length=20, null=True, blank=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=20, default='noname')),
                 ('time', models.DateTimeField()),
-                ('homework', models.CharField(max_length=40, null=True, blank=True)),
-                ('group', models.ForeignKey(to='auth.Group')),
+                ('homework', models.CharField(max_length=50, default='null')),
+                ('detail', models.TextField(default='none')),
+                ('group', models.ForeignKey(to='mopple.GroupInfo')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Penalty',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('time_arrival', models.DateTimeField()),
+                ('done_hw', models.BooleanField(default=False)),
+                ('grouping', models.ForeignKey(to='mopple.Grouping')),
+                ('meeting', models.ForeignKey(to='mopple.Meeting')),
             ],
         ),
         migrations.CreateModel(
             name='Place',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('name', models.CharField(max_length=30, null=True, blank=True)),
-                ('lnglat', models.CharField(max_length=50, null=True, blank=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(blank=True, max_length=30, null=True)),
+                ('lnglat', models.CharField(blank=True, max_length=50, null=True)),
             ],
         ),
         migrations.CreateModel(
             name='UserInfo',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
                 ('is_male', models.BooleanField(default=True)),
-                ('account_number', models.CharField(max_length=20)),
-                ('account_bank', models.CharField(max_length=10)),
-                ('balance', models.IntegerField(default=0)),
+                ('phone_num', models.CharField(max_length=20, default='none')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('account_num', models.CharField(max_length=20, default='none')),
+                ('account_bank', models.CharField(max_length=10, default='none')),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
             model_name='meeting',
             name='place',
-            field=models.ForeignKey(null=True, to='mopple.Place', blank=True),
+            field=models.ForeignKey(to='mopple.Place', blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='attendance',
-            name='meeting',
-            field=models.ForeignKey(to='mopple.Meeting'),
-        ),
-        migrations.AddField(
-            model_name='attendance',
+            model_name='grouping',
             name='user',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(to='mopple.UserInfo'),
         ),
     ]
--- mopple/models.py
+++ mopple/models.py
@@ -6,43 +6,35 @@ from datetime import timedelta
 class UserInfo(models.Model):
     user = models.OneToOneField(User)
     is_male = models.BooleanField(default=True)
-    account_number = models.CharField(default='none', max_length=20)
+    phone_num = models.CharField(max_length=20, default='none')
+    created_at = models.DateTimeField(auto_now_add=True)
+    account_num = models.CharField(default='none', max_length=20)
     account_bank = models.CharField(default='none', max_length=10)
-    balance = models.IntegerField(default=0)
-    phone_number = models.CharField(max_length=20, default='none')
 
     def __str__(self):
         return self.user.username
 
-    def get_cum_panelty(self):
-        pass
-
-
-class Standard_Attendance(models.Model):
-    standard_late = models.IntegerField()
-
-    def __str__(self):
-        return self.standard_late
-        # self.group.id != self.id
-
 
 class GroupInfo(models.Model):
     group = models.OneToOneField(Group)
-    deposit_amount = models.CommaSeparatedIntegerField(max_length=20, default=0)
-    ass_penalty = models.CommaSeparatedIntegerField(max_length=10, default=0)  # done or undone
-    att_penalty = models.CommaSeparatedIntegerField(max_length=10, default=0)  # 10분당 설정할 수 있게 / 30분 이상시 고정비
-    standard_late = models.ManyToManyField(Standard_Attendance)
-    time_start = models.DateTimeField()
-    time_end = models.DateTimeField(blank=True, null=True)
-    cuppon = models.CharField(max_length=20, default='none')
+    balance = models.IntegerField(default=0)
+    ass_rate = models.IntegerField(default=0)
+    att_rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    cuppon = models.CharField(max_length=50, default='none')
 
     def __str__(self):
         return self.group.name
         # self.group.id != self.id
 
-    def set_ass_penalty(self, time, standard_late):  # standard_late : 이 부분 dictionary 처리 해줘야하는데 잘 모르겠슴.
-        if time <= 0:
-            self.att_penalty = 0
+
+class Grouping(models.Model):
+    group = models.ForeignKey(GroupInfo)  # group.id != groupinfo.id
+    user = models.ForeignKey(UserInfo)  # user.id != userinfo.id
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.group.group.name + '/' + self.user.user.username
 
 
 class Place(models.Model):
@@ -63,57 +55,37 @@ class Place(models.Model):
         return self.name
 
 
-# 1 group N meeting
 class Meeting(models.Model):
     title = models.CharField(max_length=20, default='noname')
     time = models.DateTimeField()
     group = models.ForeignKey(GroupInfo)  # group.id != groupinfo.id
     place = models.ForeignKey(Place, blank=True, null=True)
-    homework = models.CharField(max_length=40, default='null')  # to do at that day
+    homework = models.CharField(max_length=50, default='null')  # to do at that day
     detail = models.TextField(default='none')
 
     def __str__(self):
-        return str(self.time)
-
-
-# 개인 유저와 그룹을 이어주는 테이블. 둘다 1:n 관계
-class Grouping(models.Model):
-    group = models.ForeignKey(GroupInfo)  # group.id != groupinfo.id
-    user = models.ForeignKey(UserInfo)  # user.id != userinfo.id
-    money = models.CommaSeparatedIntegerField(max_length=20, default='0')
-
-    def __str__(self):
-        return self.group.group.name + '/' + self.user.user.username
-
-
-class Deposit(models.Model):
-    grouping = models.ForeignKey(Grouping)
-    penalty = models.CommaSeparatedIntegerField(max_length=20)
-    expense = models.CommaSeparatedIntegerField(max_length=20)
-    balance = models.CommaSeparatedIntegerField(max_length=20)
-
-    def get_balance(self, penalty, expense, balance):
-        balance == self.grouping.money - penalty - expense
+        return self.group.group.name + '/' + str(self.time)
 
 
 class Penalty(models.Model):
     grouping = models.ForeignKey(Grouping)
+    meeting = models.ForeignKey(Meeting)
     time_arrival = models.DateTimeField()
     done_hw = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.user.username
+        return self.grouping.__str__() + '/' + self.meeting.__str__()
 
     def get_att_penalty(self):
         delta = self.meeting.time - self.time_arrival
-        if delta <= timedelta(0):
+        if delta >= timedelta(0):
             return 0
         else:
             penalty = abs(delta).seconds/60*self.meeting.group.att_penalty
-            return penalty
+            return round(penalty)
 
     def get_ass_penalty(self):
-        penalty = int(self.done_hw)*self.meeting.group.ass_penalty
+        penalty = int(not self.done_hw)*self.meeting.group.ass_penalty
         return penalty
 
     def islate(self):
