1a44b2918b33a78698dff155b5aba749fa85bd35
Ubuntu <ubuntu@ip-172-31-15-119.ap-northeast-1.compute.internal>
2015-11-07 07:45:03 +0000

    /signup/ json request

--- mopple/views.py
+++ mopple/views.py
@@ -1,29 +1,35 @@
 from django.shortcuts import render, redirect
 from mopple.models import *  # noqa
-# from django.contrib.auth.forms import UserCreationForm
 from mopple.forms import ProfileForm
 from django.http import HttpResponse
 import random
 from django.contrib.auth.views import login as auth_login
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
     return render(request, "index.html")
 
-
+@csrf_exempt
 def signup(request):
     if request.method == 'POST':
         form = ProfileForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            facebook_id = form.cleaned_data.get('facebook_id')
-            profile = Profile(user=user, facebook_id=facebook_id)
-            profile.save()
-            return redirect('index')
+        if request.method == 'POST':    
+            print(repr(request.body.decode('utf8')))
+            data=json.loads(request.body.decode('utf-8'))
+            a = data['facebookid']
+            b = data['username']
+            c = data['password']
+            print('a의 값은')
+            print(a)
+            print('b의 값은')
+            print(b)
+            print('c의 값은')
+            print(c)
+            return HttpResponse('post요청입니다')
     else:
-        form = ProfileForm()
-    return render(request, 'form.html', {'form': form, })
-
+        return HttpResponse('get요청입니다')
+    
 
 def sms_request(request):
     key = random.randrange(1000, 10000)
